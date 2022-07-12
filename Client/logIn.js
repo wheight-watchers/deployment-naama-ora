ce5df299bbc078c30fc0f41d43014f248a05386e
naamafrank1 <naama0556772275@gmail.com>
2022-07-13 02:57:16 +0300

    change to async-await in login instead xhr

--- Client/logIn.js
+++ Client/logIn.js
@@ -4,8 +4,8 @@ const logIn = async () => {
   debugger;
   const mail = document.getElementById("mailInput").value;
   const pswd = document.getElementById("passwordInput").value;
-  const manager_url = new URL(`https://safe-tor-83297.herokuapp.com/account/login/manager?email=${mail}&password=${pswd}`);
-  const user_url = new URL(`https://safe-tor-83297.herokuapp.com/account/login/user?email=${mail}&password=${pswd}`);
+  let manager_url = new URL(`https://safe-tor-83297.herokuapp.com/account/login/manager?email=${mail}&password=${pswd}`);
+  let user_url = new URL(`https://safe-tor-83297.herokuapp.com/account/login/user?email=${mail}&password=${pswd}`);
   debugger;
   try {
     let manager = await fetch(manager_url);
@@ -18,10 +18,10 @@ const logIn = async () => {
   }
   catch (error) {
     try {
-      const user = await fetch(user_url);
-      if (user) {
-        let jsonUser = await user.json()
-        window.location.href = `../Client/User.html?userId=${jsonUser.id}`;
+      let user = await fetch(user_url);
+    //  user = await user.json()
+      if (user.status===200) {
+        window.location.href = `../Client/User.html?userId=${pswd}`;
       }
     } catch (err) {
       alert("not found!!!")
