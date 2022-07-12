59887bc0be0a2c41cc354d6bdc4c9718da2a197f
naamafrank1 <naama0556772275@gmail.com>
2022-07-12 19:39:56 +0300

    constructorgit add .!

--- Client/logIn.js
+++ Client/logIn.js
@@ -3,8 +3,8 @@ const logIn = async () => {
   debugger;
   const mail = document.getElementById("mailInput").value;
   const pswd = document.getElementById("passwordInput").value;
-  const manager_url = `https://safe-tor-83297.herokuapp.com/account/login/manager?email=${mail}&password=${pswd}`;
-  const user_url = `https://safe-tor-83297.herokuapp.com/account/login/user?email=${mail}&password=${pswd}`;
+  const manager_url =new URL(`https://safe-tor-83297.herokuapp.com/account/login/manager?email=${mail}&password=${pswd}`) ;
+  const user_url =new URL(`https://safe-tor-83297.herokuapp.com/account/login/user?email=${mail}&password=${pswd}`) ;
   debugger;
   const XHR = new XMLHttpRequest();
   XHR.open("GET", manager_url);
@@ -14,10 +14,7 @@ const logIn = async () => {
       alert(`Error ${XHR.status}: ${XHR.statusText}`);
     } else {
       let manager =
-        //  JSON.parse(
         XHR.response;
-      //  );
-      //  if(mail==manager.email&&pswd==manager.password)
       if (manager) {
         if (window.confirm("welcome")) {
           window.location.href = "../Client/Manager.html";
@@ -31,14 +28,8 @@ const logIn = async () => {
             alert(`Error ${userXHR.status}: ${userXHR.statusText}`);
           } else {
             let user =
-              // JSON.parse(
               userXHR.response;
-            // );
-            // CurrentUser = users.find((u) => u.email == mail && u.id == pswd);
-            // if (CurrentUser) {
             if (user) {
-              //   localStorage.setItem("cu", JSON.stringify(CurrentUser)); //זה רק בנוסף
-              //   debugger;
               let jsonuser = JSON.parse(userXHR.response);
               window.location.href = `../Client/User.html?userId=${jsonuser.id}`;
             } else alert("not found");
