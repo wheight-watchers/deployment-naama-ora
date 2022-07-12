e9c00138c515a2c657e30520088d4a74673698f5
naamafrank1 <naama0556772275@gmail.com>
2022-07-12 11:11:41 +0300

    construcror

--- Client/diaryManagement.js
+++ Client/diaryManagement.js
@@ -157,18 +157,11 @@ function addInputForSnack() {
   }
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function () {
   modal.style.display = "none";
 };
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function (event) {
-//   debugger;
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
+//TODO
 function addDayToDiary() {
   debugger;
   const dateInput = document.getElementById("dateInput").value;
