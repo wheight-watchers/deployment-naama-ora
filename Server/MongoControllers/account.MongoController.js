abeaab2498e885152dc0f05fd1e799a8a7e88375
naamafrank1 <naama0556772275@gmail.com>
2022-07-18 02:15:07 +0300

    add mongo-controller for account

---
+++ Server/MongoControllers/account.MongoController.js
@@ -0,0 +1,19 @@
+const db = require('../DB/dataBase');
+const { ObjectId } = require('mongodb');
+
+module.exports = {
+    userLogin: async function (req, res) {
+        const userLogin = req.query;
+        const { email, password } = userLogin;
+        const doc = {$set: { email:email,password:password} };
+        const login_user=await db.getDB().collection("account").findOne(doc);
+        res.send(`user ${login_user} welcome!!!🤩`)
+    },
+    managerLogin: async function (req, res) {
+        const managerLogin = req.query;
+        const { email, password } = managerLogin;
+        const doc = {$set: { email:email,password:password}  };
+        const login_user=await db.getDB().collection("account").findOne(doc);
+        res.send(`manager ${login_user} welcome!!!🤑`)
+    }
+}
