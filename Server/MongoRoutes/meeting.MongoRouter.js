170690f5e0e113f0b8021ab031dcb753ba6bced7
naamafrank1 <naama0556772275@gmail.com>
2022-07-18 01:35:18 +0300

    add mongo-router for meeting

---
+++ Server/MongoRoutes/meeting.MongoRouter.js
@@ -0,0 +1,9 @@
+const express= require('express');
+const controller=require('../MongoControllers/meeting.MongoController')
+
+const router=express.Router();
+
+router.get('/:id',controller.getAllTheMeetingsForUser);
+router.post('/',controller.addMeeting);
+router.put('/:id',controller.updateMeeting);
+router.delete('/:id',controller.deleteMeeting);
