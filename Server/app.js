0714be3d53270bbdd32ad18b545ecbe707c47a7b
OraBenesh <orabenesh@gmail.com>
2022-07-17 13:59:27 +0300

    env

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const db = require('./DB/mongoose')
 const app = express();
 const cors = require("cors");
@@ -7,11 +7,13 @@ const userRouter = require("./Routes/user.router");
 const meetingRouter = require("./Routes/meeting.router");
 const accountRouter = require("./Routes/account.router");
 // const authMiddleware = require("./MiddleWare/middleware");
-const logger = require('./Log/logger');
+// const logger = require('./Log/logger');
 const port = process.env.PORT || 3000;
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 var bodyParser = require('body-parser')
+const dotenv = require('dotenv');
+dotenv.config();
 db.connect();
 app.use(cors());
 app.use(express.json());
