299c5d2ec9783faca45ffbd0c6a6eec3f781b70c
naamafrank1 <naama0556772275@gmail.com>
2022-07-18 01:50:26 +0300

    add mongo-controller for diary

--- Server/MongoControllers/meeting.MongoController.js
+++ Server/MongoControllers/meeting.MongoController.js
@@ -1,4 +1,5 @@
 const db = require('../DB/dataBase');
+const { ObjectId } = require('mongodb');
 module.exports = {
     getAllTheMeetingsForUser: async (req, res) => {
         const id = req.params.id;
@@ -8,18 +9,18 @@ module.exports = {
     addMeeting: async function (req, res) {
         if (req.body) {
             const meeting = req.body;
-            const inserted = await db.getDB().collection("users").insertOne(meeting);
+            const inserted = await db.getDB().collection("meeting").insertOne(meeting);
             res.send(req.body)
         }
     },
     deleteMeeting: async function(req,res){
-        const meeting = await db.getDB().collection("users").deleteOne({ _id: ObjectId(req.params.id) });
+        const meeting = await db.getDB().collection("meeting").deleteOne({ _id: ObjectId(req.params.id) });
         res.send(`delete user ${meeting}`)
     },
     updateMeeting: async function(req,res){
         const meetingToUpdate = req.body;
         const filter = { _id: ObjectId(req.params.id) };
-        const meeting = await db.getDB().collection("users").updateOne(filter, meetingToUpdate);
+        const meeting = await db.getDB().collection("meeting").updateOne(filter, meetingToUpdate);
         res.send(`user ${meeting} updated!`)
     }
 }
