aac0b310a71671dce54b75aadb7a5d6c2935c5b2
naamafrank1 <naama0556772275@gmail.com>
2022-07-18 01:52:20 +0300

    add mongo-router for diary

---
+++ Server/MongoRoutes/diary.MongoRouter.js
@@ -0,0 +1,9 @@
+const express= require('express');
+const controller=require('../MongoControllers/diary.MongoController')
+
+const router=express.Router();
+
+router.get('/:id/diary',controller.getDiaryByUserId);
+router.post('/:id/diary',controller.addDiary);
+router.put('/:id/diary/:id',controller.updateDiary);
+router.delete('/:id/diary/:id',controller.deleteDairy);
