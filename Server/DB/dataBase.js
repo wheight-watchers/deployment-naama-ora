dc23603d5b54baeb5a7ce3f478b07658fd49452a
OraBenesh <orabenesh@gmail.com>
2022-07-17 22:47:25 +0300

    save added

--- Server/DB/dataBase.js
+++ Server/DB/dataBase.js
@@ -1,12 +1,10 @@
 const { MongoClient } = require('mongodb');
 const connectionString = "https://safe-tor-83297.herokuapp.com";
-
 class dataBase {
 
   constructor() {
 
   }
-
   async connect() {
     const client = new MongoClient(connectionString, {
       useNewUrlParser: true,
@@ -15,12 +13,11 @@ class dataBase {
 
     let connected = await client.connect();
     Logger.setLevel("debug");
-    this.db = connected.db("MyFirstmongoDB");
+    this.db = connected.db("weight-watchers");
     await db.command({ hello: 1 });
 
     console.log("DB Connected!")
   };
-
   getDB() {
     return this.db;
   }
--- Server/DB/mongoose.js
+++ Server/DB/mongoose.js
@@ -4,7 +4,7 @@ class MongooseDB {
     constructor() { }
     async connect() {
         // const url=`mongodb://localhost:27017`;
-        const url = `mongodb://localhost:${process.env.HOST || 27017}/${process.env.DB || ""}`;
+        const url = `mongodb://localhost:${process.env.HOST || 27017}/${process.env.DB || "weight-watchers"}`;
         await mongoose.connect(url);
         console.log(`mongoose DB connected!`);
     };
