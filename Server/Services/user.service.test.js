68ae101c84d2294c1265cde6eb268262023f1012
OraBenesh <orabenesh@gmail.com>
2022-07-18 13:08:11 +0300

    for pull

--- Server/Services/user.service.test.js
+++ Server/Services/user.service.test.js
@@ -1,6 +1,15 @@
-const userService = require('./user.service');
-const UserService = require('./user.service.test');
+const UserService = require('./user.service');
 
-test('get all users', () => {
-  expect(userService.getUser(1)).toBe(JSON.parse());
+test('user with id 1 to be 1', () => {
+  expect(UserService.getUser("1")).toBe(1);
 });
+// const sum = require('./sum');
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+// describe("Calculator tests", () => {
+    // test('adding 1 + 2 should return 3', () => {
+    //   expect(mathOperations.sum(1, 2)).toBe(3);
+    // });
+//    })
