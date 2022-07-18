e4a50080ff8b6fb54f00597c568ad34ba122acc1
OraBenesh <orabenesh@gmail.com>
2022-07-18 13:55:21 +0300

    shorter cheching syntax

--- Server/Services/user.service.test.js
+++ Server/Services/user.service.test.js
@@ -2,11 +2,7 @@
 const UserService = require('./user.service');
 
 test('user with id 1 to be 1', () => {
-    async function getUser(id){
-        const user = await UserService.getUser(id);
-        return user;
-    }
-    let user=getUser("1").then(()=>{
-  expect(JSON.parse(user)).toBe({"id":1,"password":1,"firstName":"Ora","lastName":"Benesh","email":"ob@gmail.com","address":{"city":"maale-Adumim","street":"bialic","building":"5"},"height":"1.7","age":"33","Weights":{"startWeight":77,"meetings":[{"id":1,"date":"12/04/2020","weight":75},{"id":2,"date":"18/04/2020","weight":75},{"id":4,"date":"12/04/2020","weight":10}]},"diary":[{"date":"12/04/2020","summary":[{"Breakfast":["roll","chocolate-milk"]},{"Lunch":["rice","meet","beans"]},{"Dinner":["baget","salad","omlete"]},{"IntermediateSnack":["coffee","chocolate","cookies"]}]},{"date":"13/04/2020","summary":[{"Breakfast":["roll","chocolate-milk"]},{"Lunch":["rice","meet","beans"]},{"Dinner":["baget","salad","omlete"]},{"IntermediateSnack":["coffee","chocolate","cookies"]}]},{"date":"14/04/2020","summary":[{"Breakfast":["banana","apple"]},{"Lunch":["rice","meet","beans"]},{"Dinner":["baget","salad","omlete"]},{"IntermediateSnack":["coffee","chocolate","cookies"]}]}]});
-    });
+let user =  UserService.getUser("1").then(()=>{
+    expect(JSON.parse(user)).toBe({"id":1,"password":1,"firstName":"Ora","lastName":"Benesh","email":"ob@gmail.com","address":{"city":"maale-Adumim","street":"bialic","building":"5"},"height":"1.7","age":"33","Weights":{"startWeight":77,"meetings":[{"id":1,"date":"12/04/2020","weight":75},{"id":2,"date":"18/04/2020","weight":75},{"id":4,"date":"12/04/2020","weight":10}]},"diary":[{"date":"12/04/2020","summary":[{"Breakfast":["roll","chocolate-milk"]},{"Lunch":["rice","meet","beans"]},{"Dinner":["baget","salad","omlete"]},{"IntermediateSnack":["coffee","chocolate","cookies"]}]},{"date":"13/04/2020","summary":[{"Breakfast":["roll","chocolate-milk"]},{"Lunch":["rice","meet","beans"]},{"Dinner":["baget","salad","omlete"]},{"IntermediateSnack":["coffee","chocolate","cookies"]}]},{"date":"14/04/2020","summary":[{"Breakfast":["banana","apple"]},{"Lunch":["rice","meet","beans"]},{"Dinner":["baget","salad","omlete"]},{"IntermediateSnack":["coffee","chocolate","cookies"]}]}]});
+})
 });
