53beccc1eaeda2305015f4e81b46cf6b448426a1
naamafrank1 <naama0556772275@gmail.com>
2022-07-19 13:13:47 +0300

    start

--- Server/MongoControllers/meeting.MongoController.js
+++ Server/MongoControllers/meeting.MongoController.js
@@ -1,10 +1,16 @@
-const db = require('../DB/dataBase');
 const { ObjectId } = require('mongodb');
+const meetingModel = require('../Models/users.schema')
+
 module.exports = {
     getAllTheMeetingsForUser: async (req, res) => {
-        const id = req.params.id;
-        const meetings = await db.getDB().collection("users").findOne(ObjectId(id));
-        res.send(`get meeting ${meetings}`)
+        try {
+            let id = req.params.id;
+            const meetings = await meetingModel.findById(ObjectId(id));
+            res.send(`get meeting ${meetings}`)
+        }catch(error){
+            res.send(`🙄oops ${error}`)
+        }
+        
     },
     addMeeting: async function (req, res) {
         if (req.body) {
@@ -13,11 +19,11 @@ module.exports = {
             res.send(req.body)
         }
     },
-    deleteMeeting: async function(req,res){
+    deleteMeeting: async function (req, res) {
         const meeting = await db.getDB().collection("users").deleteOne({ _id: ObjectId(req.params.id) });
         res.send(`delete user ${meeting}`)
     },
-    updateMeeting: async function(req,res){
+    updateMeeting: async function (req, res) {
         const meetingToUpdate = req.body;
         const filter = { _id: ObjectId(req.params.id) };
         const meeting = await db.getDB().collection("users").updateOne(filter, meetingToUpdate);
