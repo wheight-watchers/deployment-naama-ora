38991dad18938b42c8b5772292497b2935d02eda
OraBenesh <orabenesh@gmail.com>
2022-07-19 13:39:52 +0300

    diary

--- Server/Models/diary.schema.js
+++ Server/Models/diary.schema.js
@@ -3,12 +3,12 @@ const { Schema } = mongoose;
 
 const diarySchema = new Schema({
     date: { type: String, required: true },
-    summary: [
-        { BreakFast: { type: [String]} },
-        { Lunch: { type: [String]} },
-        { Dinner: { type: [String]} },
-        { IntermediateSnack: { type: [String]} }  
-    ]
+    summary: {
+     BreakFast: { type: [String] },
+    Lunch: { type: [String] },
+    Dinner: { type: [String] },
+    IntermediateSnack: { type: [String] }
+ }
 })
 const DiaryModel = mongoose.model('Diary', diarySchema);
 module.exports = { DiaryModel, diarySchema };
