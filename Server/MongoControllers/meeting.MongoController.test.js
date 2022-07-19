3963a8441f16c0523fc476a0c8627ef000651c0a
naamafrank1 <naama0556772275@gmail.com>
2022-07-19 18:23:41 +0300

    test of meeting controller mongo

--- Server/MongoControllers/meeting.MongoController.test.js
+++ Server/MongoControllers/meeting.MongoController.test.js
@@ -1,4 +1,36 @@
 const meetingController = require('./meeting.MongoController');
 const UsersModel = require('../Models/users.schema')
 
-describe("meeting with Mongo test", () => { })
+const { expect } = require("chai");
+const sinon = require("sinon");
+describe("meeting with Mongo test", () => {
+    afterEach(() => {
+        sinon.restore();
+    })
+    test('get all meeting for user by user id', async () => {
+        const id = 1;
+        const allMeeting =
+            [
+                {
+                    "id": 1,
+                    "date": "12/04/2020",
+                    "weight": 65
+                },
+                {
+                    "id": 2,
+                    "date": "18/04/2020",
+                    "weight": 78
+                },
+                {
+                    "id": 4,
+                    "date": "12/04/2020",
+                    "weight": 60
+                }
+
+            ]
+        sinon.stub(UsersModel, 'getAllTheMeetingsForUser').returns(allMeeting);
+        const returnMeeting = await meetingController.getAllTheMeetingsForUser(id)
+        expect(returnMeeting).to.equal(allMeeting)
+
+    })
+})
