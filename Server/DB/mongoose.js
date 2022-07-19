c048b20639125ee5f8324eab81c6cc43e9035dbd
naamafrank1 <naama0556772275@gmail.com>
2022-07-19 03:47:37 +0300

    return 500- ooooooooooooooooooooooof

--- Server/DB/mongoose.js
+++ Server/DB/mongoose.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 class MongooseDB {
     constructor() { }
     async connect() {
-        // const url=`mongodb://localhost:27017`;
-        const url = `mongodb://localhost:${process.env.HOST || 27017}/${process.env.DB || "weight-watchers"}`;
+         const url=`mongodb://localhost:27017/weight_watchers`
+     
+       // const url = `mongodb://localhost:${process.env.HOST || 27017}/${process.env.DB || "weight-watchers"}`;
         await mongoose.connect(url,{
             useNewUrlParser: true,
             // useFindAndModify: false,
